8db175110b3e851c7945acf0daaff99f6526d28f
Ryan <rbass6@uncc.edu>
2023-09-26 13:19:39 -0400

    Added existing code

---
+++ src/Menu/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+function Menu() {
+  return (
+    <nav className="menu" role="navigation">
+        <ul>
+            <li><Link to="/">Homepage</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/login">Login</Link></li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Menu;
